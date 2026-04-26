afef1d46cd1840781d9d0e18bca1b6309f718d68
chanchlesh0407 <173004141+chanchlesh0407@users.noreply.github.com>
2026-04-27 01:27:53 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL;;
+const BASE_URL = "https://electricity-bill-calculator-production.up.railway.app/api";
 
 // 🔥 UNIVERSAL RESPONSE HANDLER
 const handleResponse = async (res) => {
