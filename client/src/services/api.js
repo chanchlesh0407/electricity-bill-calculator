6caccbed0b1b394d4c0d089f889c1c06801eb770
chanchlesh0407 <173004141+chanchlesh0407@users.noreply.github.com>
2026-04-26 06:07:21 +0530

    Update BASE_URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://electricity-bill-calculator-wyh5.onrender.com";
 
 // 🔥 UNIVERSAL RESPONSE HANDLER
 const handleResponse = async (res) => {
